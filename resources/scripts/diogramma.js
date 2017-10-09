247b57e698a087fad39046a1bbfadf58b24a6642
Lpkehoke <ibymif2000@yandex.ru>
2017-10-09 20:41:37 +0300

    secondgit status

--- resources/scripts/diogramma.js
+++ resources/scripts/diogramma.js
@@ -1,41 +1,45 @@
 function create( num , color) {
-  if(drawingCanvas && drawingCanvas.getContext) {
-  var context = drawingCanvas.getContext('2d');
-  // рисуем окружность
-  context.fillStyle = "#ddd";
-  context.strokeStyle = "#ddd";
-  context.beginPath();
-  context.arc(80,80,80,0,Math.PI*2,true);
-  context.closePath();
-  context.fill();
+	if(drawingCanvas && drawingCanvas.getContext) {
+		var context = drawingCanvas.getContext('2d');
+		// рисуем окружность
+		context.fillStyle = "#ddd";
+		context.strokeStyle = "#ddd";
+		context.beginPath();
+		context.arc(80, 80, 80, 0, Math.PI*2, true);
+		context.closePath();
+		context.fill();
 
-  // рисуем сектор окружности num%
+		// рисуем сектор окружности num%
 
-  context.fillStyle = color;
-  context.beginPath();
-  context.moveTo(80, 80);
-  //координаты старта определяем так чтоб закрашенная область всегда была снизу
-  var start=(Math.PI/180)*270;
-  context.arc(80,80,80,start,start+(Math.PI/180)*num*360/100,false);
-  context.closePath();
-  context.fill();
+		context.fillStyle = color;
+		context.beginPath();
+		context.moveTo(80, 80);
 
-  // закрашиваем внутреннюю окружность меньшего радиуса
-  context.fillStyle = "#F8F8F8";
-  context.beginPath();
-  context.arc(80,80,67,0,Math.PI*2,true);
-  context.closePath();
-  context.fill();
+		//координаты старта определяем так чтоб закрашенная область всегда была снизу
 
-  // пишем текст
-  context.fillStyle = "#3c4761";
-  context.font = "50px Titillium Web";
-  context.textAlign = "center";
-  context.textBaseline = 'middle';
-  var x=drawingCanvas.width/2;
-  var y=drawingCanvas.height/2;
-  context.fillText(num+"%", x,y);
-  }
+		var start = (Math.PI / 180) * 270;
+		context.arc(80, 80, 80, start, start+(Math.PI/180)*num*360/100, false);
+		context.closePath();
+		context.fill();
+
+		// закрашиваем внутреннюю окружность меньшего радиуса
+
+		context.fillStyle = "#F8F8F8";
+		context.beginPath();
+		context.arc(80, 80, 67, 0, Math.PI*2, true);
+		context.closePath();
+		context.fill();
+
+		// пишем текст
+
+		context.fillStyle = "#3c4761";
+		context.font = "50px Titillium Web";
+		context.textAlign = "center";
+		context.textBaseline = 'middle';
+		var x = drawingCanvas.width / 2;
+		var y = drawingCanvas.height / 2;
+		context.fillText(num+"%", x, y);
+	}
 }
 
 var drawingCanvas = document.getElementById('dg1');
